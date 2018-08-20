dba662d2985407d29c058e446a8975330504980a
ed588 <calvin.588.edwards@gmail.com>
2018-08-20 12:00:06 +0100

    add database getting code

---
+++ nselec/db.py
@@ -0,0 +1,16 @@
+from tinydb import TinyDB
+from flask import g, current_app
+
+def get_db():
+    if 'db' not in g:
+        g.db = TinyDB(current_app.config['DATABASE'])
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db) # close the db at the end of the request
+    
