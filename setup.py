08359422baee28d6e7a4e074b2b736cf5cff625d
ed588 <calvin.588.edwards@gmail.com>
2018-08-20 12:05:33 +0100

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nselec',
+    version='2.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'requests',
+        'tinydb',
+    ],
+)
