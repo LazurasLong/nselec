e215278afe339eb324f28de0d63dc6120a594f70
ed588 <calvin.588.edwards@gmail.com>
2018-08-31 12:26:04 +0100

    default voters

--- nselec/__init__.py
+++ nselec/__init__.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask
 
+def noconf_voters_example_data():
+    # you should put a function called GET_VOTERS in the config file
+    # this function is the backup
+    return {"testlandia":"Testlandia"}
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = "dev",
         DATABASE = os.path.join(app.instance_path, 'nselec.db'),
+        GET_VOTERS = noconf_voters_example_data,
     )
 
     if test_config is None:
