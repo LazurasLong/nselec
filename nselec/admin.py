a166b81a383690a886e7734ee43c8f96e3aba08c
ed588 <calvin.588.edwards@gmail.com>
2018-10-21 19:46:12 +0100

    you can now edit ranked elections

--- nselec/admin.py
+++ nselec/admin.py
@@ -49,19 +49,8 @@ def delete_election(el_id):
         return redirect(url_for("admin.elections"))
     return render_template("admin/delete_election.html", el=el)
 
-@bp.route("/elections/actual_delete/<int:el_id>")
-@login_required
-def actual_delete(el_id):
-    db = get_db()
-    el = db.get(doc_id=el_id)
-    if el is None:
-        abort(404)
-    tt = time_type(el['times']['start'], el['times']['end'])
-    if tt != "future":
-        abort(404)
-    return redirect(url_for("admin.elections"))
 
-@bp.route("/elections/edit/<int:el_id>")
+@bp.route("/elections/edit/<int:el_id>", methods=["GET", "POST"])
 @login_required
 def edit_election(el_id):
     db = get_db()
@@ -71,75 +60,97 @@ def edit_election(el_id):
     tt = time_type(el['times']['start'], el['times']['end'])
     if tt != "future":
         abort(404)
-    return render_template("admin/edit_election.html", el=el)
 
+    if request.method == "POST":
+        if el['type'] == "ranked":
+            succ, data = get_data_ranked()
+            if succ:
+                db.remove(doc_ids=[el_id])
+                db.insert(data)
+                flash("Election updated successfully!", "success")
+                return redirect(url_for("admin.elections"))
+            else:
+                flash(data, "error")
+    else:
+        if el['type'] == "ranked":
+            return render_template("admin/edit_ranked.html", el=el)
 
 @bp.route("/elections/new/yesno", methods=["GET", "POST"])
 @login_required
 def new_yesno():
     if request.method == "POST":
-        print(request.form)
-        name = request.form['name']
-        desc = request.form['desc']
-        start = request.form['start_date']+" "+request.form['start_time']
-        end = request.form['end_date']+" "+request.form['end_time']
-        fstr = "%Y-%m-%d %H:%M"
-        start_dt = datetime.strptime(start, fstr)
-        end_dt = datetime.strptime(end, fstr)
-        start_f = FancyTime(start_dt)
-        end_f = FancyTime(end_dt)
-        db = get_db()
-        payload = {
-            "name": name,
-            "desc": desc,
-            "times": {
-                "start": start_f,
-                "end": end_f
-            },
-            "type": "yesno",
-            "voters": [],
-            "votes": {
-                "for": 0,
-                "against": 0
-            }
-        }
-        db.insert(payload)
-        flash("Successfully added new election!", "success")
-        return redirect(url_for("admin.elections"))
+        succ, data = get_data_yesno()
+        if succ:
+            db = get_db()
+            db.insert(data)
+            flash("Successfully added new election!", "success")
+            return redirect(url_for("admin.elections"))
+        else:
+            flash(data, "error")
     return render_template("admin/new_yesno.html")
 
 @bp.route("/elections/new/ranked", methods=["GET", "POST"])
 @login_required
 def new_ranked():
     if request.method == "POST":
-        name = request.form['name']
-        desc = request.form['desc']
-        start = request.form['start_date']+" "+request.form['start_time']
-        end = request.form['end_date']+" "+request.form['end_time']
-        fstr = "%Y-%m-%d %H:%M"
-        start_dt = datetime.strptime(start, fstr)
-        end_dt = datetime.strptime(end, fstr)
-        start_f = FancyTime(start_dt)
-        end_f = FancyTime(end_dt)
-        db = get_db()
-        options = request.form.getlist('opt')
-        if len(options) < 2:
-            flash("You need at least 2 options", "error")
-        else:
-            payload = {
-                "name":name,
-                "desc":desc,
-                "times": {
-                    "start":start_f,
-                    "end":end_f
-                },
-                "voters": [],
-                "votes": [],
-                "options": options,
-                "type": "ranked"
-            }
-            db.insert(payload)
+        succ, data = get_data_ranked()
+        if succ:
+            db = get_db()
+            db.insert(data)
             flash("Successfully added election!", "success")
             return redirect(url_for('admin.elections'))
-
+        else:
+            flash(data, "error")
     return render_template("admin/new_ranked.html")
+
+
+def get_data_yesno():
+    name = request.form['name']
+    desc = request.form['desc']
+    start = request.form['start_date']+" "+request.form['start_time']
+    end = request.form['end_date']+" "+request.form['end_time']
+    fstr = "%Y-%m-%d %H:%M"
+    start_dt = datetime.strptime(start, fstr)
+    end_dt = datetime.strptime(end, fstr)
+    start_f = FancyTime(start_dt)
+    end_f = FancyTime(end_dt)
+    return True, {
+        "name": name,
+        "desc": desc,
+        "times": {
+            "start": start_f,
+            "end": end_f
+        },
+        "type": "yesno",
+        "voters": [],
+        "votes": {
+            "for": 0,
+            "against": 0
+        }
+    }
+
+def get_data_ranked():
+    name = request.form['name']
+    desc = request.form['desc']
+    start = request.form['start_date']+" "+request.form['start_time']
+    end = request.form['end_date']+" "+request.form['end_time']
+    fstr = "%Y-%m-%d %H:%M"
+    start_dt = datetime.strptime(start, fstr)
+    end_dt = datetime.strptime(end, fstr)
+    start_f = FancyTime(start_dt)
+    end_f = FancyTime(end_dt)
+    options = request.form.getlist('opt')
+    if len(options) < 2:
+        return False, "You need at least 2 options"
+    return True,  {
+        "name":name,
+        "desc":desc,
+        "type": "ranked",
+        "times": {
+            "start":start_f,
+            "end":end_f
+        },
+        "voters": [],
+        "votes": [],
+        "options": options,
+    }
