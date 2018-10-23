cb6b220e57ddcf004efcf22e7945a396db3e5346
ed588 <calvin.588.edwards@gmail.com>
2018-10-23 17:22:15 +0100

    permissions code

--- nselec/auth.py
+++ nselec/auth.py
@@ -12,17 +12,52 @@ from tinydb import Query
 
 from nselec.db import get_db
 
+def check_user():
+    if "user" not in session:
+        # not even logged in
+        return False, "You need to be logged in"
+    db = get_db()
+    User = Query()
+    if not db.table("users").contains(User.username == session['user']):
+        # that user doesn't exist!
+        # it shouldn't be possible for somebody to tamper with the session, this
+        # is mainly to log people out if the user gets deleted.
+        return False, "The user you are logged in as does not exist (any more)"
+    return True, "seems ok to me"
+
 def login_required(view):
     # decorator for views/pages for which the user needs to be logged in
     # view is the view function we're decorating
     @functools.wraps(view)
     def wrapped(**kwargs):
-        if "user" not in session:
-            flash("You need to be logged in to do that.", "error")
+        ok, msg = check_user()
+        if not ok:
+            flash(msg, "error")
             return redirect(url_for('auth.login'))
         else:
             return view(**kwargs)
     return wrapped
+
+def role_required(role):
+    # decorator for views that need a role value in the db greater than or equal
+    # to the value passed to the db.
+    # currently we have 0 (user) and 1 (admin) but this may change in future.
+    def inner(view):
+        view = login_required(view)
+        @functools.wraps(view)
+        def wrapped(**kwargs):
+            db = get_db()
+            # we know the user exists, because login_required should check that for us
+            User = Query()
+            u = db.table("users").get(User.username == session['user'])
+            if u['role'] >= role:
+                return view(**kwargs)
+            else:
+                flash("You do not have permissions to do that.", "error")
+                return redirect(url_for("admin.index"))
+        return wrapped
+    return inner # that's a lot of nested functions but I think it makes sense
+
 
 bp = Blueprint("auth", __name__)
 
@@ -50,11 +85,6 @@ def signup():
             flash(error, "error")
     return render_template("auth/signup.html")
 
-@bp.route("/d/<s>")
-def d(s):
-    return TimestampSigner(current_app.secret_key).sign(s)
-
-
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -66,13 +96,13 @@ def login():
         User = Query()
         entry = usertab.get(User.username == username)
         if entry == None or not check_password_hash(entry.get('password', None), password):
-            flash("Invalid username or password!")
+            flash("Invalid username or password!", "error")
         else:
             # valid login, hooray!
             session.clear()
             session['user'] = username
             flash("Login successful!", "success")
-            return redirect(url_for('index'))
+            return redirect(url_for('admin.index'))
     return render_template("auth/login.html")
 
 @bp.route("/logout")
