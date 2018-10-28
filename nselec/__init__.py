4e08bf62571f64477a9c26e13b7e181be62f86b8
ed588 <calvin.588.edwards@gmail.com>
2018-10-28 11:23:53 +0000

    apparently this is a change

--- nselec/__init__.py
+++ nselec/__init__.py
@@ -5,20 +5,22 @@ __version__ = "2.0rc0"
 
 from flask import Flask
 
+
 def noconf_voters_example_data():
     # you should put a function called GET_VOTERS in the config file
     # this function is the backup
-    return {"testlandia":"Testlandia"}
+    return {"testlandia": "Testlandia"}
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = "dev",
-        DATABASE = os.path.join(app.instance_path, 'nselec.db'),
-        GET_VOTERS = noconf_voters_example_data,
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "nselec.db"),
+        GET_VOTERS=noconf_voters_example_data,
     )
 
-    app.config.from_pyfile('config.py', silent=True)
+    app.config.from_pyfile("config.py", silent=True)
 
     try:
         os.makedirs(app.instance_path)
@@ -26,28 +28,24 @@ def create_app():
         pass
 
     from . import db
+
     db.init_app(app)
 
     from . import election_list
+
     app.register_blueprint(election_list.bp)
 
-    modules = (
-        "vote",
-        "results",
-        "auth",
-        "admin",
-        "pages",
-    )
+    modules = ("vote", "results", "auth", "admin", "pages")
 
     for modname in modules:
-        mod = importlib.import_module("nselec."+modname)
+        mod = importlib.import_module("nselec." + modname)
         bp = mod.bp
-        app.register_blueprint(bp, url_prefix="/"+modname)
+        app.register_blueprint(bp, url_prefix="/" + modname)
 
-    app.add_url_rule('/', endpoint="index")
+    app.add_url_rule("/", endpoint="index")
 
     @app.context_processor
     def cp():
-        return {"version":__version__}
+        return {"version": __version__}
 
     return app
